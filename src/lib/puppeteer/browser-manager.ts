5ec97ac3b3161897dce0b78241cdbef419e3f7e5
Israeligal <israeligal2@gmail.com>
2025-06-16 12:17:35 +0300

    fix: update Chromium executable path for Puppeteer in browser manager
    - Changed the executable path for Chromium in `browser-manager.ts` to a new download link, ensuring the correct binary is utilized for Puppeteer. This update enhances the reliability of the browser setup and continues the effort to maintain an accurate Puppeteer integration.

--- src/lib/puppeteer/browser-manager.ts
+++ src/lib/puppeteer/browser-manager.ts
@@ -30,7 +30,7 @@ export async function getBrowser() {
   try {
     browser = await puppeteerCore.launch({
       args: [...chromium.args, ...args],
-      executablePath: await chromium.executablePath("https://www.dropbox.com/scl/fi/b7stjtchpnu7nphms7dd4/chromium-v137.0.0-pack.arm64.tar?rlkey=0bt7t9l1xsujbble6wzs915y7&st=bh73tkhz&dl=1"), // Use default chromium binary
+      executablePath: await chromium.executablePath("https://www.dropbox.com/scl/fi/uvep7tdr557j8vmjtzzlq/chromium-v137.0.0-pack.x64.tar?rlkey=zh4jtxa4vbvn5ohm2jn8dv25f&st=7uhkqwcg&dl=1"), // Use default chromium binary
       headless: "shell", // Required for Puppeteer with v137.0.0
       defaultViewport: viewport,
     });
